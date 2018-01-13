babcecd5f0f9468a2bde73184ffc5c8b657fb366
nataliaIv <nataliaivanova_83@abv.bg>
2018-01-13 14:09:53 +0200

    Homework13

---
+++ Nataly_Iv/Homework13/script.js
@@ -0,0 +1,49 @@
+
+//1 zad.
+var a = 50;
+var result = Math.pow(a,2);
+console.log(result);
+
+//2 zad.
+var b = 51;
+var number = Math.pow(b,3);
+console.log(number);
+
+//3 zad.
+var r = 8;
+var perimeter = 2*Math.PI*r;
+console.log(perimeter);
+
+//4 zad.
+var r2 = 8;
+var pi = 3.14;
+var perimeter2 = 2*pi*r2;
+console.log(perimeter2);
+
+//5 zad.
+function findArea(a, b, c) {
+    var areaRectangle = a*b;
+    var areaTriangle = a*(c-b)/2;
+    var allArea = areaRectangle + areaTriangle;
+    return allArea;
+}
+
+//6 зад.
+//var myNumber = 255;
+//var myHexNumber = myNumber.toString(16);
+//console.log(myHexNumber); // => ff
+
+//function colorRGB (r, g, b) {
+//    var r = r.toString(16);
+//    var g = g.toString(16);
+//    var b = b.toString(16);
+//    console.log();
+//}
+// 
+//colorRGB(255, 192, 253);
+
+
+
+
+
+
